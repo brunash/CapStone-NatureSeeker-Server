61ef8be37d0ec7607fcaddc1800352f5f870046d
brunash <bobazso@yahoo.com>
2022-03-31 22:42:03 -0400

    added database

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const app = express();
+const dotenv =              require('dotenv');
+const express =             require('express');
+const session =             require('express-session');
+const expressHandlebars =   require('express-handlebars');
+const mongoose  =           require('mongoose');
+const passport =            require('passport');
+const localStrategy =       require('passport-local').Strategy;
+const bcrypt =              require('bcrypt');
+const app =                 express();
 dotenv.config();
-const cors = require('cors');
-const userData = require('./data/userData')
+const cors =                require('cors');
+const userData =            require('./data/userData');
+const connect = require('passport/lib/framework/connect');
+const connectDb = require('./config/db');
+
+mongoose.connect
+connectDb()
+
 
 app.get('/', (req, res) => {
     res.send('API is running..');
